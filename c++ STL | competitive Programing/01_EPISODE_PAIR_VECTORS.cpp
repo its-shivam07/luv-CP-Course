b4705d6d9b6e8305f3a4dec748fe8451ef144934
shivam parashar <122305190+its-shivam07@users.noreply.github.com>
2024-08-06 13:22:44 +0530

    Update 01_EPISODE_PAIR_VECTORS.cpp

--- c++ STL | competitive Programing/01_EPISODE_PAIR_VECTORS.cpp
+++ c++ STL | competitive Programing/01_EPISODE_PAIR_VECTORS.cpp
@@ -19,3 +19,64 @@ int main() {
     cout << p_array[i].first << " " <<p_array[i].second << endl;
   }
 }
+
+
+// ----------------------------------------
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printvec(vector< string > &v) {
+  cout << "size:" << v.size() << endl;
+  for (int i = 0; i < v.size(); i++) {
+    // v.size() ->0(1)
+    cout << v[i] << " ";
+  }
+  //v.push_back(2);
+  cout << endl;
+}
+
+int main() {
+
+  // // input string 
+  vector < string > v;
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {    
+    string s;
+    cin >> s;
+    v.push_back(s);
+  }
+  printvec(v);
+
+  // // declaration
+  // vector<int> v(10, 3); // size 10, all elements are 3
+  // v.push_back(5);
+  // printvec(v);
+  // v.pop_back(); 
+  // printvec(v); //0(1))
+
+  
+  // // direct make copy of vector 
+  // vector <int> v;
+  // v.push_back(7);
+  // v.push_back(6);
+  
+  // vector <int> v2 =v ; //o(n) like for loop so use refrence &v2 to avoid copying
+  // v2.push_back(5);
+  // printvec(v);
+  // printvec(v2);
+
+  // // VECTOR IS DYNAMIC SIZED ARRAY
+  // vector<int> v;
+  // int n;
+  // cin>>n;
+  // for(int i=0;i<n;i++){
+  //   int x;
+  //   cin>>x;
+  //   v.push_back(x);
+
+  // }
+  // printvec(v);
+}
